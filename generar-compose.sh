21f14d62cce0a6c32d81ecd970414cc14f2daa6c
belfortep <pbelforte2003@gmail.com>
2025-03-18 16:07:55 -0300

    Added script for compose-generator

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Nombre del archivo de salida: $1"
-echo "Cantidad de clientes: $2"
-python3 mi-generador.py $1 $2
+echo "Output filename: $1"
+echo "Number of clients: $2"
+python3 ./compose-generator/compose_generator.py $1 $2
