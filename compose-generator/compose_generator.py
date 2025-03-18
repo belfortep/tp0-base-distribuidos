09f66685f815167ff60732dc932889644f4df389
belfortep <pbelforte2003@gmail.com>
2025-03-18 17:19:18 -0300

    Added volumes to compose_generator.py

--- compose-generator/compose_generator.py
+++ compose-generator/compose_generator.py
@@ -13,6 +13,8 @@ services:
       - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
+    volumes:
+      - ./server/config.ini:/config.ini
     """
 
     client_template = """
@@ -27,6 +29,8 @@ services:
       - testing_net
     depends_on:
       - server
+    volumes:
+      - ./client/config.yaml:/config.yaml
     """
 
     for i in range(1, number_of_clients + 1):
