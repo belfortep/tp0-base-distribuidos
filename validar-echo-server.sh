77cfc0a1759f89136553b33fd527ea7f1e70026a
belfortep <pbelforte2003@gmail.com>
2025-03-19 15:35:16 -0300

    tests pass!

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-TEST_MESSAGE="TEST"
-RESPONSE=$(docker run --rm --network="tp0_testing_net" alpine sh -c "echo $TEST_MESSAGE | nc server 12345")
+TEST_MESSAGE="De aquel amor, De musica ligera..."
+PORT="12345"
+RESPONSE=$(docker run --rm --network="tp0_testing_net" alpine sh -c "echo $TEST_MESSAGE | nc server $PORT")
 
 if [ "$RESPONSE" = "$TEST_MESSAGE" ]; then
-    echo "action: test_echo_server | result: success"
+    RESULT="success"
 else
-    echo "action: test_echo_server | result: fail"
+    RESULT="fail"
 fi
+
+echo "action: test_echo_server | result: $RESULT"
