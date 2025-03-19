733d870a2c3cb8de47dbcd590095ee1ed8acc81f
belfortep <pbelforte2003@gmail.com>
2025-03-19 15:28:36 -0300

    Fixed comparsion operator

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -3,7 +3,7 @@
 TEST_MESSAGE="TEST"
 RESPONSE=$(docker run --rm --network="tp0_testing_net" alpine sh -c "echo $TEST_MESSAGE | nc server 12345")
 
-if [ "$RESPONSE" == "$TEST_MESSAGE" ]; then
+if [ "$RESPONSE" = "$TEST_MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
