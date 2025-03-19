79fcd3ce79a1fdda3365dde240f06b181f3ffc80
belfortep <pbelforte2003@gmail.com>
2025-03-19 15:21:41 -0300

    changed to alpine

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESPONSE=$(docker run --rm --network="tp0_testing_net" sh gophernet/netcat -c "echo TEST | nc server 12345")
+RESPONSE=$(docker run --rm --network="tp0_testing_net" alpine sh -c "echo TEST | nc server 12345")
 
 
 echo $RESPONSE
