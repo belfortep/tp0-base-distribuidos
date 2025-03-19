2d2cf6e0cd7d410e7b48c2e4708b2ecfac15f4bc
belfortep <pbelforte2003@gmail.com>
2025-03-19 15:20:06 -0300

    Fixing problems

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESPONSE=$(docker run --rm --network="tp0_testing_net" gophernet/netcat sh -c "echo TEST | nc server 12345")
+RESPONSE=$(docker run --rm --network="tp0_testing_net" sh gophernet/netcat -c "echo TEST | nc server 12345")
 
 
 echo $RESPONSE
