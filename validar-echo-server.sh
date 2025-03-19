a833b2126881d24c8121fb6b645ccc81c0535961
belfortep <pbelforte2003@gmail.com>
2025-03-19 15:24:28 -0300

    added verification of msg

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,5 +2,8 @@
 
 RESPONSE=$(docker run --rm --network="tp0_testing_net" alpine sh -c "echo TEST | nc server 12345")
 
-
-echo $RESPONSE
+if [ "$RESPONSE" == "TEST" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
