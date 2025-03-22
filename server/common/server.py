83e07c1a0aac9480297dccf21881fd36e12efbd3
belfortep <pbelforte2003@gmail.com>
2025-03-22 19:45:57 -0300

    fixed error of bad len

--- server/common/server.py
+++ server/common/server.py
@@ -57,8 +57,9 @@ class Server:
     
     def __read_bet(self):   
         message = read_up_to_delimiter(self._last_client_socket, b'\0')
+        logging.info(f"action: reading_bet | result: success | message {message}")
         data_list = message.split(";")
-        if len(data_list) != 5:
+        if len(data_list) != 6:
             raise Exception("Corrupted message read")
         
         agency = data_list[0]
