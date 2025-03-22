c480589376f6bfd3f14f3e4e8546b9a00b2339e4
belfortep <pbelforte2003@gmail.com>
2025-03-22 20:01:42 -0300

    easier send for server

--- server/common/connection.py
+++ server/common/connection.py
@@ -1,11 +1,6 @@
 def send(connection, message):
-    byte_to_write = len(message)
-    bytes_already_written = 0
-    while bytes_already_written < byte_to_write:
-        bytes_written = connection.send(message[bytes_already_written:].encode('utf-8'))   
-        if bytes_written == 0:
-            raise Exception("Socket is closed in sending")
-        bytes_already_written = bytes_already_written + bytes_written
+
+    connection.sendall(message.encode("utf-8"))
 
 
 def read_up_to_delimiter(connection, delimiter):
