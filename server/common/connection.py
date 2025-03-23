6ed74316fc5722f51a05579c75946f4246171385
belfortep <pbelforte2003@gmail.com>
2025-03-23 08:14:40 -0300

    fix typo

--- server/common/connection.py
+++ server/common/connection.py
@@ -5,18 +5,18 @@ def send(connection, message):
 
 def read_up_to_delimiter(connection, delimiter):
     buffer = bytearray()
-    fund_delimiter = False
+    found_delimiter = False
     delimiter = delimiter.encode("utf-8")
     delimiter_index = 0
 
-    while not fund_delimiter:
-        chunk = connection.recv(3)
+    while not found_delimiter:
+        chunk = connection.recv(1024)
         if not chunk:
             raise Exception("Socket is closed in reading")
         buffer.extend(chunk)
 
         delimiter_index = buffer.find(delimiter)
         if delimiter_index != -1:
-            fund_delimiter = True
+            found_delimiter = True
 
     return buffer[:delimiter_index].decode("utf-8")
