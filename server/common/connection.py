513ca445ad5194a4f4459abc86cdfe35e52b6e39
belfortep <pbelforte2003@gmail.com>
2025-03-23 08:11:07 -0300

    changed to use delimiter_index

--- server/common/connection.py
+++ server/common/connection.py
@@ -7,14 +7,16 @@ def read_up_to_delimiter(connection, delimiter):
     buffer = bytearray()
     fund_delimiter = False
     delimiter = delimiter.encode("utf-8")
+    delimiter_index = 0
 
     while not fund_delimiter:
-        chunk = connection.recv(1024)
+        chunk = connection.recv(3)
         if not chunk:
             raise Exception("Socket is closed in reading")
         buffer.extend(chunk)
 
-        if delimiter in chunk:
+        delimiter_index = buffer.find(delimiter)
+        if delimiter_index != -1:
             fund_delimiter = True
 
-    return buffer[:-len(delimiter)].decode("utf-8")
+    return buffer[:delimiter_index].decode("utf-8")
